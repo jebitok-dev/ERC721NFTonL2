243b86a2fcccf8f6650746d197faf7d7dab109a3
Sharon Jebitok <sharonkosgei4@gmail.com>
2022-05-19 23:01:57 +0300

    Initialize Contract

---
+++ contracts/nftpg.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+
+contract Greeter {
+    
+}
---
+++ hardhat.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  solidity: "0.8.10",
+  networks: {
+    mumbai: {
+      url: process.env.TESTNET_RPC,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY
+  }
+};;
